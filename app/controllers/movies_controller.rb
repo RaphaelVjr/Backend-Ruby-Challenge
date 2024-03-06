d4c1b0fc8e05aeeba7dacd09d061daa288ea8062
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-03-06 16:53:38 -0300

    movie csrf

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :authenticate_user!, only: [:create]
   require 'csv'
   require 'json'
